fb3b96a8439d864fdae48c8dafc3af341ca4b9fe
jaisondavid-m <jaison7373@gmail.com>
2025-10-17 23:29:35 +0530

    nav css path change

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
-import { useState } from 'react'
-import NavBar from './Component/NavBar/Navbar'
-import Home from './Pages/Home/Home'
-import { Route,Routes,Navigate } from 'react-router-dom'
-import Social9 from './Pages/Social-9th/Social9'
-import Social10 from './Pages/Social-10th/Social10'
-import Nmms from './Pages/Nmms/Nmms'
-import Footer from './Component/Footer/Footer'
+  import { useState } from 'react'
+  import NavBar from './Component/NavBar/Navbar'
+  import Home from './Pages/Home/Home'
+  import { Route,Routes,Navigate } from 'react-router-dom'
+  import Social9 from './Pages/Social-9th/Social9'
+  import Social10 from './Pages/Social-10th/Social10'
+  import Nmms from './Pages/Nmms/Nmms'
+  import Footer from './Component/Footer/Footer'
 
-function App() {
+  function App() {
 
-  return(
-    <div>
-      <NavBar/>
-      <Routes>
-         <Route path='/' element={<Navigate to="/home"/>}/>
-         <Route path='/home' element={<Home/>}/>
-         <Route path='/nmms' element={<Nmms/>}/>
-         <Route path='/social-9th' element={<Social9/>}/>
-         <Route path='/social-10th' element={<Social10/>}/>
-      </Routes>
-      <Footer/>
-    </div>
-  )
-}
+    return(
+      <div>
+        <NavBar/>
+        <Routes>
+          <Route path='/' element={<Navigate to="/home"/>}/>
+          <Route path='/home' element={<Home/>}/>
+          <Route path='/nmms' element={<Nmms/>}/>
+          <Route path='/social-9th' element={<Social9/>}/>
+          <Route path='/social-10th' element={<Social10/>}/>
+        </Routes>
+        <Footer/>
+      </div>
+    )
+  }
 
-export default App
+  export default App
--- src/Component/NavBar/Navbar.jsx
+++ src/Component/NavBar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./NavBar.css";
+import "./Navbar.css";
 import { Link } from "react-router-dom";
 
 function NavBar() {
